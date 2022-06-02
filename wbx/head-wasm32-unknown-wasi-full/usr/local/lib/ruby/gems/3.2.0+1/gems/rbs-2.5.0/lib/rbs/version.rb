efc2eba9282d2d9719fd08a23b352a699181097a
Daniel Phillips <danieledward.phillips@gmail.com>
2022-06-01 19:22:24 -0500

    WIP ruby wasm install, sans caching strategy

---
+++ wbx/head-wasm32-unknown-wasi-full/usr/local/lib/ruby/gems/3.2.0+1/gems/rbs-2.5.0/lib/rbs/version.rb
@@ -0,0 +1,3 @@
+module RBS
+  VERSION = "2.5.0"
+end
--- wbx/src/main.rs
+++ wbx/src/main.rs
@@ -3,6 +3,9 @@
 use std::process::Command;
 use indicatif::ProgressBar;
 use clap::Parser;
+use reqwest::Client;
+use std::path::Path;
+use std::io::Cursor;
 
 #[derive(Parser)]
 struct Cli {
@@ -16,12 +19,77 @@ fn main() {
     let content = std::fs::read_to_string(&args.path)
         .expect("could not read file");
 
-    for line in content.lines() {
-       println!("{}", line);
-    }
+    command()
+}
+
+fn command() {
+
+    // 1. understand context (lang ecosystem), and have relevant tooling downloaded for that
+    //     - can also pass in if used outside of language package environment, i.e. just installed
+    //     or built from source (eventually, would be great to do wasm-releases as the default):
+    //     this should be walked through, in the nice waw vue-cli or rust installation works :)
+    //
+
+    // # Download a prebuilt Ruby release
+    //$ curl -LO https://github.com/kateinoigakukun/ruby.wasm/releases/download/2022-03-28-a/ruby-head-wasm32-unknown-wasi-full.tar.gz
+    //$
+
+    //  check if exists locally, in some kinda cache or something, else
 
     progress();
-    command();
+
+    // todo: check local cache for built wasm ruby
+    if !Path::new("/etc/hosts").exists() {
+      println!("exists");
+    } else {
+      get("https://github.com/ruby/ruby.wasm/releases/download/2022-06-01-a/ruby-head-wasm32-unknown-wasi-full.tar.gz".to_string(), "wasm-ruby-latest.gz".to_string());
+    }
+
+    //Command::new("ruby")
+        //.arg("-e")
+        //.arg("puts 'hi'")
+        //.status()
+        //.expect("ls command failed to start");
+
+    Command::new("tar xfz wasm-ruby-latest.tar.gz")
+    //# Extract ruby binary not to pack itself
+    //$ mv  ruby.wasm
+    Command::new("mv head-wasm32-unknown-wasi-full/usr/local/bin/ruby ruby.wasm")
+
+    // app source code for test
+    Command::new("mkdir src; echo "'puts 'hello'" > src/my_app.rb")
+
+    //# Pack the whole directory under /usr and your app dir
+    Command::new("wasi-vfs pack ruby.wasm --mapdir /src::./src --mapdir /usr::./head-wasm32-unknown-wasi-full/usr -o my-ruby-app.wasm")
+
+    // combine all the steps above, and then add Gem bundling mechanism
+    //   - needs to detect with type/version of ruby is installed in dir, and get matching prebuilt binary ?
+    //
+    // 3. make bundling work bundle: https://gist.github.com/kateinoigakukun/5caf3b83b2732b1653e91b0e75ce3390
+    //
+    // 4. Make built application split out at /wasm-dist/<entrypoint-script>.wasm
+    //
+    // 5. able to do `wbx run <entrypoint-script>.wasm` which invokes either the default or specified runtime (wasmtime)
+    //
+    // Entire rubywasm + source code pushed to registry (possibly, bindle)
+}
+
+fn get(url: String, file_name: String) -> Result<(), Box<dyn std::error::Error>> {
+
+    let mut res = reqwest::blocking::get(url)?;
+
+    let mut file = std::fs::File::create(file_name)?;
+    let mut content =  Cursor::new(res.bytes()?);
+    std::io::copy(&mut content, &mut file)?;
+    Ok(())
+
+    //println!("Status: {}", res.status());
+    //println!("Headers:\n{:?}", res.headers());
+
+    //res.copy_to(&mut std::io::stdout())?;
+
+    //println!("\n\nDone.");
+    //Ok(())
 }
 
 fn progress() {
@@ -32,79 +100,3 @@ fn progress() {
     }
     pb.finish_with_message("done, but cant see this shit");
 }
-
-fn command() {
-    //Command::new("ruby")
-        //.arg("-e")
-        //.arg("puts 'hi'")
-        //.status()
-        //.expect("ls command failed to start");
-
-    // 1. understand context (a pip lib, or an gem ), and have relevant tooling downloaded for that
-    //     - can also pass in if used outside of language package environment, i.e. just installed
-    //     or built from source (eventually, would be great to do wasm-releases as the default):
-    //     this should be walked through, in the nice waw vue-cli or rust installation works :)
-    //
-    // 2.  # Download a prebuilt Ruby release
-    // # Download a prebuilt Ruby release
-            //$ curl -LO https://github.com/kateinoigakukun/ruby.wasm/releases/download/2022-03-28-a/ruby-head-wasm32-unknown-wasi-full.tar.gz
-            //$ tar xfz ruby-head-wasm32-unknown-wasi-full.tar.gz
-
-            //# Extract ruby binary not to pack itself
-            //$ mv head-wasm32-unknown-wasi-full/usr/local/bin/ruby ruby.wasm
-
-            //# Put your app code
-            //$ mkdir src
-            //$ echo "puts 'Hello'" > src/my_app.rb
-
-            //# Pack the whole directory under /usr and your app dir
-            //$ wasi-vfs pack ruby.wasm --mapdir /src::./src --mapdir /usr::./head-wasm32-unknown-wasi-full/usr -o my-ruby-app.wasm
-            //
-            //# Run the packed scripts
-            //$ wasmtime my-ruby-app.wasm -- /src/my_app.rb
-            //Hello
-    //
-    //
-    //   - needs to detect with type of ruby is installed in dir, and get matching prebuilt binary ?
-    //
-    // 3. make bundling work bundle: https://gist.github.com/kateinoigakukun/5caf3b83b2732b1653e91b0e75ce3390
-    //
-    // 4. Make built application split out at /wasm-box/<entrypoint-script>.wasm
-    //
-    // 5. able to do `wbx run <entrypoint-script>.wasm` which invokes either the default or specified runtime (wasmtime)
-
-    //let script_path = "tests/sample.rb";
-
-    //Command::new("ruby")
-        //.arg(script_path)
-        //.status()
-        //.expect("ls command failed to start");
-        //.arg("puts 'hi'")
-        //
-    //println!("\n {} compiled to WebAssembly at ./wasm_box/what_to_call_this.wasm", script_path);
-    //println!("\n Note: this has been compiled AOT to run server-side, using WASI to manage system calls (more info: https://wasi.dev). Running elsewhere (like in a browser) may not be the best experience with this compilation");
-   // println!("\n Execute your compiled {} by typing ./wasm_box/what_to_call_this.wasm (and hitting enter) from your current directory\n", script_path);
-}
-
-//ideas from wasm-boxer repo
-//`wbx script.rb` aliases wbx build <script>
-
-//`wbx script.rb --web`
-
-//`wbx script.rb --runtime wasmtime` // or choose any other runtime (this makes sure runtime is installed at least)
-
-//Ex. a Rails app:
-
-//`wbx .`
-
-// wbx run /built.wasm or just cd into ./build/ and do wbx run (obv this is kinda like docker run)
-
-//show difference in system usage/size, etc. once you do this
-
-//### Box up your project to run in almost any environment.
-
-//`Default `wbx` compiles AOT to Wasm (no runtime needed -- HOPEFULLY). The output is a .wasm file that can run on any [WebAssembly Runtime](https://github.com/appcypher/awesome-wasm-runtimes), or be Ahead-of-Time compiled.
-
-//show benchmarks compared to containers vms: start up times, size, native vs wasm excution, etc. tons and tons of good visualizations.
-
-
